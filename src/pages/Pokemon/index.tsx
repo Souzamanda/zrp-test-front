d00ccbc43ae0c5750abcc484997e6f4073fa71af
Amanda Puttomatti <acsputtomatti@gmail.com>
2025-03-28 04:28:28 -0300

    chore: added base styling for dex page

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,7 +1,11 @@
 export default function Pokemon() {
   return (
-    <div className='bg-red-100'>
-      <h1 className='text-3xl'>Pokemon</h1>
+    <div className='bg-primary h-screen flex items-center justify-center'>
+      <div className='bg-border p-10 rounded-lg rounded-bl-4xl'>
+        <div className='bg-secondary w-2xl rounded-md p-3'>
+          <h1 className='text-3xl font-display text-center'>Pokemon</h1>
+        </div>
+      </div>
     </div>
   )
 }
