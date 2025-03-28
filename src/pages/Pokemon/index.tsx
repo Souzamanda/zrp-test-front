e8adb5599373fdc0624031ebbe110393684cc181
Amanda Puttomatti <acsputtomatti@gmail.com>
2025-03-28 07:48:34 -0300

    chore: added logic to search new pokemon

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,45 +1,52 @@
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import { usePokemon } from '../../hooks/pokemon'
 import { useEffect, useState } from 'react'
 import { PokemonDTO } from '../../types/PokemonDTO'
-import {
-  FaRegCheckCircle,
-  FaRegTimesCircle,
-} from 'react-icons/fa'
+import { FaRegCheckCircle, FaRegTimesCircle } from 'react-icons/fa'
 
 export default function Pokemon() {
   const [pokemonData, setPokemonData] = useState<PokemonDTO>()
+  const [pokemonInput, setPokemonInput] = useState<string>('')
+
   const { getPokemon } = usePokemon()
+  const navigate = useNavigate()
 
   const pokemonName = useParams().pokemonName
 
+  function onSubmit() {
+    navigate(`/${pokemonInput}`)
+  }
+
   useEffect(() => {
     if (pokemonName) {
-      getPokemon(pokemonName).then((response) => {
-        //console.log(response?.data.abilities)
-        const { name, abilities, types, sprites } = response?.data
-        setPokemonData({
-          name: name || '',
-          abilities: abilities || [],
-          types: types || [],
-          sprites: {
-            front_default: sprites.front_default || '',
-            front_shiny: sprites.front_shiny || '',
-          },
+      setPokemonInput('')
+      getPokemon(pokemonName)
+        .then((response) => {
+          //console.log(response?.data.abilities)
+          const { name, abilities, types, sprites } = response.data
+          setPokemonData({
+            name: name || '',
+            abilities: abilities || [],
+            types: types || [],
+            sprites: {
+              front_default: sprites.front_default || '',
+              front_shiny: sprites.front_shiny || '',
+            },
+          })
         })
-      })
+        .catch((error) => {
+          console.error('Erro ao buscar o Pokémon:', error)
+        })
     }
   }, [pokemonName])
-
-  if (pokemonData) {
-    console.log(pokemonData)
-  }
 
   return (
     <div className='bg-primary h-screen flex items-center justify-center flex-col gap-6'>
       <div className='bg-border p-15 rounded-lg rounded-bl-4xl shadow-lg w-2xl'>
         <div className='bg-secondary rounded-md p-3'>
-          <div className='flex justify-center gap-10 my-5'>
+          {pokemonData ? (
+            <>
+            <div className='flex justify-center gap-10 my-5'>
             <div className='flex flex-col items-center'>
               <img
                 src={pokemonData?.sprites.front_default}
@@ -85,6 +92,13 @@ export default function Pokemon() {
               ))}
             </ul>
           </div>
+          </>
+          ) : (
+            <div className='h-70 flex items-center justify-center'>
+              <h1>Carregando pokemon...</h1>
+            </div>
+          )}
+          
         </div>
       </div>
 
@@ -98,16 +112,20 @@ export default function Pokemon() {
           <textarea
             className='bg-input w-full h-full rounded-md p-2 outline-0 placeholder:text-gray-800 font-body font-bold'
             placeholder='Digite o nome do pokemon'
+            onChange={(e) =>
+              setPokemonInput(e.target.value.toLocaleLowerCase())
+            }
+            value={pokemonInput}
           />
         </div>
         <div className='flex gap-5 py-3 pr-3'>
-          <button className='self-start'>
+          <button className='self-start' onClick={() => setPokemonInput('')}>
             <FaRegTimesCircle
               size={70}
               className='fill-cancel stroke-20 stroke-red-900 bg-cancel rounded-full'
             />
           </button>
-          <button className='self-end'>
+          <button className='self-end' onClick={() => onSubmit()}>
             <FaRegCheckCircle
               size={70}
               className='fill-confirm stroke-20 stroke-green-800 bg-confirm rounded-full'
