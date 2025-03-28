d98b5b9d6f4edeca3c8e25ee5a79abbd5c627737
Amanda Puttomatti <acsputtomatti@gmail.com>
2025-03-28 10:00:15 -0300

    chore: changed header styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,16 @@ import BottomDisplay from '../../components/BottomDisplay'
 export default function Home() {
   return (
     <div className='bg-primary sm:h-screen h-full w-full flex items-center justify-center flex-col gap-6'>
+      <div className='flex gap-5 w-full max-w-2xl ml-5'>
+        <div className='w-16 h-16 bg-white rounded-full flex justify-center items-center'>
+          <span className='w-13 h-13 bg-header-button rounded-full' />
+        </div>
+        <div className='flex gap-2'>
+          <span className='w-4 h-4 bg-red-600 rounded-full border-red-900 border-1 mt-2' />
+          <span className='w-4 h-4 bg-yellow-300 rounded-full border-yellow-900 border-1 mt-2' />
+          <span className='w-4 h-4 bg-green-500 rounded-full border-green-900 border-1 mt-2' />
+        </div>
+      </div>
       <div className='bg-border px-15 py-8 rounded-xl rounded-bl-[70px] shadow-lg w-full max-w-2xl'>
       <div className='mb-7 flex justify-center gap-10'>
           <span className='w-4 h-4 bg-red-600 rounded-full' />
