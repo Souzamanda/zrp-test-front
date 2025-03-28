006df02f7cc40eb8acbedc2c1e9d66f369a32440
Amanda Puttomatti <acsputtomatti@gmail.com>
2025-03-28 03:08:01 -0300

    chore: did initial tailwind and page configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Pokemon from './pages/Pokemon'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/pokemon' element={<Pokemon />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
---
+++ src/pages/Pokemon/index.tsx
@@ -0,0 +1,7 @@
+export default function Pokemon() {
+  return (
+    <div className='bg-red-100'>
+      <h1 className='text-3xl'>Pokemon</h1>
+    </div>
+  )
+}
